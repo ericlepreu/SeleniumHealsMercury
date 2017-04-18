5370a7073cdc6e8813f8bd5ce0e98f60732b786a
Eric <Eric@Eric-PC.frontier.com>
2017-04-18 16:47:10 -0400

    skeleton of BaseTest working

--- src/main/java/com/mercury/travel/utils/BaseTest.java
+++ src/main/java/com/mercury/travel/utils/BaseTest.java
@@ -2,9 +2,11 @@ package com.mercury.travel.utils;
 
 import java.io.FileInputStream;
 import java.text.SimpleDateFormat;
+import java.util.Enumeration;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -13,28 +15,34 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public abstract class BaseTest {
 	protected WebDriver driver = new ChromeDriver();
+	protected static String sharedLocation = "c:/Eclipse/AutomatedTests";
 	
 	protected static Logger logger;
 	protected static Properties prop = new Properties();
 	protected static String filePath; 
+	protected static String envir;
+	protected static String appDomain;
 	
 	
 	@BeforeClass
 	public static void setUpClass() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "C:\\Eclipse\\Selenium\\ChromeDriver2.29\\chromedriver.exe");
 		
-//		//load properties file
-//		prop.load(new FileInputStream("config.properties"));
-//		
-//		//set the local properties
-//		setLocalProperties();
-//		
-//		//set the file path
-//		setFilePath();
-//		
-//		//initialize the logger
-//		setupLogger();
-//		logger.info("Logger is setup ------------------------- ");
+		//load properties file
+		prop.load(new FileInputStream("config.properties"));
+		
+		//set the local properties
+		setLocalProperties();
+		
+		//set the file path
+		setFilePath();
+		
+		//initialize the logger
+		setupLogger();
+		//logger.info("Logger is setup ------------------------- ");
+		
+		//Determine App Domain and Selenium Grid instance
+		setupAppDomain();
 		
 		
 	}
@@ -54,7 +62,7 @@ public abstract class BaseTest {
 	 */
 	private static void setLocalProperties() {
 		if(prop.getProperty("local_environment") != null) {
-			setLocalProperty("applicationDomain");
+			setLocalProperty("appDomain");
 			//setLocalProperty("local_filePath");
 		}
 	}
@@ -76,21 +84,59 @@ public abstract class BaseTest {
 	 * for now just setup the file path for logging results for just running local
 	 */
 	private static void setFilePath() {
+		
 		//for now just running local
-		filePath = prop.getProperty("local_filePath");
+		filePath = sharedLocation + prop.getProperty("local_filePath");
 	}
 	
 	/**
-	 * Initialize the log4j loggger. If the system property is empty (not provided 
+	 * Initialize the log4j logger. If the system property is empty (not provided 
 	 * write to the local logs directory; otherwise log to a particular environment folder (QA, SRT, etc)
 	 */
-	public static void setupLogger() {
-		//use the test method name + time stamp
-		StackTraceElement[] ste = Thread.currentThread().getStackTrace();
-		String testMethodName = ste[ste.length - 1].getMethodName();  
-		//  see http://stackoverflow.com/questions/442747/getting-the-name-of-the-current-executing-method
-		String timeStamp = new SimpleDateFormat("yyyy_MM_dd.HH_mm_ss").format(new java.util.Date());
+	private static void setupLogger() {
+		String testMethodName = "TestLog_";
+		String timeStamp = new SimpleDateFormat("yyyy_MM_dd_HHmmss").format(new java.util.Date());
 		System.setProperty("logfilename", testMethodName + timeStamp);		
+		
+		DOMConfigurator.configure("log4j.xml");
+		
+		//initialize logger
+		logger = Logger.getLogger(filePath + "/logs/" + System.getProperty("logfilename"));
+				
+		logger.info("log file created: " + filePath + "/logs/" + System.getProperty("logfilename"));
 	}
 
+	/**
+	 * Setup the AppDomain using the System.Property 'environment'
+	 */
+	private static void setupAppDomain() throws Exception {
+		Properties properties = System.getProperties();
+		Enumeration<?> e = properties.propertyNames();
+		
+//		//Only use code block below for debugging
+//		logger.info("System property keys =====================");
+//		while(e.hasMoreElements()) {
+//			String key = (String) e.nextElement();
+//			logger.info("System Property: " + key + " has value: " + properties.getProperty(key));
+//		}
+		
+		String systemPropEnv = System.getProperty("environment");
+		
+		logger.info("System env var 'environment' is: " + systemPropEnv);
+		//if System.getProperty("environment") is null, use the domain based on local_environment specified in config.properties
+		if(systemPropEnv == null || "".equals(systemPropEnv.trim())) {
+			envir = "LOCAL";
+			logger.info("Using settings specified by 'local_environment' in config.properties, since System 'environment' variable is null");
+			logger.info("Local Envir settings are set to: " + prop.getProperty("local_environment"));				
+			
+			appDomain = prop.getProperty("local_appDomain");
+			logger.info("App Domain set to: " + appDomain + " ====================");
+		} else if("QA".equals(systemPropEnv) || "SRT".equals(systemPropEnv)) {
+			envir = systemPropEnv;
+			logger.info("Using settings for " + envir + ", since System Property 'environment' is set to QA");
+			logger.info("Domain set to: " + prop.getProperty(envir + "_appDomain"));	
+		} else {
+			throw new Exception("Error: System was not set to one of the legit values: null, '', QA or SRT");
+		}
+	}
 }
